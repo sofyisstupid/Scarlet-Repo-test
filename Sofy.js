f96bc6009dc605824c385955c0cbf08f905833cc
sofyisstupid <143369993+sofyisstupid@users.noreply.github.com>
2023-08-28 04:07:41 +0100

    Update Sofy.js

--- Sofy.js
+++ Sofy.js
@@ -1,19 +1,25 @@
 {
-    "name": "Sofy Repo",
-    "version": "0.1",
-    "down": "link_to_file",
-    "dev": "Sofy",
-    "icon": "link_to_app_icon",
-    "category": "gaming lol",
-    "description": "Description of application",
-    "bundleID": "app.bundle.id",
-    "screenshots" [
-        "link_to_screenshot1",
-        "link_to_screenshot2",
-        "link_to_screenshot3"
-    ],
-    "contact": {
-        "web": "developer_website",
-        "twitter": "https://twitter.com/developer"
+  "META": {
+    "repoName": "Scarlet Repo",
+    "repoIcon": "https://usescarlet.com/favicon.png"
+  },
+  "Games": [
+    {
+      "name": "iPogo",
+      "version": "0.279.3",
+      "down": "https://ipogo.app/download/ipogo-3.0.6.ipa",
+      "category": "Hacked Games",
+      "description": "With iPogo you get a full set of tools to help you be the best. iPogo is a Simple, Powerful and Intuitive App.",
+      "bundleID": "com.nianticlabs.pokemongo",
+      "appstore": "com.nianticlabs.pokemongo"
+    },
+    {
+      "name": "SpooferX",
+      "version": "0.279.3",
+      "down": "https://www.spooferx.app/ipa/SpooferPro-0.279.3-0.194.ipa",
+      "category": "Hacked Games",
+      "changelog": "- Added Support for Latest version\n- Fixed Scarlet install error",
+      "description": "SpooferX for Pok\u00e9mon GO is an enhanced edition of the Pok\u00e9mon GO app, adding a joystick and with free teleport ! It also include a lot of others features : IV checking, enhanced throw, auto walking and GPX, fast catch trick, a live feed and even a map with everything around you !",
+      "bundleID": "com.nianticlabs.pokemongo",
+      "appstore": "com.nianticlabs.pokemongo"
     }
-}
